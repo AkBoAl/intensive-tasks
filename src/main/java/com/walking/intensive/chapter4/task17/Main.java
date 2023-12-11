35320396fecd1d4c0610c196eebd0421d08078f7
Boris <aksenov.boris8595@gmeil.com>
2023-12-12 01:16:08 +0500

    Видел как другие рекурсию сюда пропихивали, но на вход передавали 2 аргумента: массив и его длину. Есть ли вариант с рекурсией, но без второго аргумента?

--- src/main/java/com/walking/intensive/chapter4/task17/Main.java
+++ src/main/java/com/walking/intensive/chapter4/task17/Main.java
@@ -1,7 +1,23 @@
 package com.walking.intensive.chapter4.task17;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] array = {0, 5, 2, 1, 5, -1, 8, -2};
+        System.out.println(Arrays.toString(getBubbleSort(array)));
+    }
 
+    private static int[] getBubbleSort(int[] array) {
+        for (int i = array.length - 1; i >= 0; i--) {
+            for (int j = 0; j <= i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int bubble = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = bubble;
+                }
+            }
+        }
+        return array;
     }
 }
